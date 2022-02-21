d125b5ed3844994f4004b07a7f753c7c581c58a4
gowtham-ss-ragavan <ssgowtham21@gmail.com>
2022-02-21 07:25:00 -0800

    Generic thresholding support
    - Allows keeping a specified number of singular values while
    thresholding.
    - Intended for TLS version of Companion DMD

--- src/function_forge.m
+++ src/function_forge.m
@@ -6,6 +6,26 @@
  or Wolfram/Debug.
 *)
 
+genericpickysvd[x_, sigpicker_] :=
+    Module[ {usv, rawsigmas, sigmas, condandterror},
+        (*Get SVD of x*)
+        (*UpTo: To compute all Subscript[\[Sigma],i] so that we can threshold ourselves*)
+        usv = SingularValueDecomposition[x, UpTo@Min[Dimensions[x]]];
+        (*Here we choose the singular values that are considered and those that are neglected*)
+        rawsigmas = Diagonal[usv[[2]]];
+        sigmas = sigpicker@rawsigmas;
+        (*Find unthresholded and thresholded \[Kappa](x) and the largest Subscript[\[Sigma],i] not included*)
+        condandterror = getcondandtail[rawsigmas, Length@sigmas];
+        (*Choose the corresponding rows of U^H and V^H*)
+        {thinsvd[usv, sigmas], condandterror}
+    ];
+
+pickysvd[x_, sigtols_] := 
+  genericpickysvd[x, sigthresholder[#, sigtols] &];
+
+lowrankapprox[x_, sigpicker_] := 
+  usvdot@((genericpickysvd[x, sigpicker])[[1]]);
+
 (* Removed tinit -  Carries noise too *)
 
 ms1shot4trajvariations[trajgrubIP_, liftgrub_,  crunchgrub_, {rawicsIP_,noiseIP_, valselem_}] := Module[{trajgrubOP},
@@ -22,53 +42,56 @@ datapartsflavs = prep4cases[data,\[Lambda],truevals,sigtols, restols,nfunda, nde
 MapThread[getcstar[#1,\[Lambda],sigtols,restols,#2,#3,crows,ndelays]&,datapartsflavs]\[Transpose](* [[Attributes, Cases ]]*)
 ];
 
-prep4cases[data_,\[Lambda]_,truevals_,sigtols_,restols_,nfunda_, ndelays_, deg_, meff_, noisyQ_]:=
-Module[{zmat,zlambdams,zlambdamsdel,n,evalparts,flavours,dataorgs},
-n = deg;
-zmat =  Take[ Transpose[   data[[1]]  ],-(n+1)]//Transpose ;
+prep4cases[data_, \[Lambda]_, truevals_, sigtols_, restols_, nfunda_, ndelays_, deg_, meff_, noisyQ_] :=
+    Module[ {zmat, zlambdams, zlambdamsdel, n, evalparts, flavours, dataorgs},
+        n = deg;
+        zmat = Take[Transpose[data[[1]]], -(n + 1)] // Transpose;
+        zlambdams = getmusubeddatamat[zmat, \[Lambda]];
+        zlambdamsdel = 
+         Join @@ Map[#[zlambdams\[Transpose]]\[Transpose] &, {Most, Rest}];
+        evalparts = 
+         getevalpartitions[truevals, nfunda, n, \[Lambda], restols];
+        flavours = {"vanilla", "ms", "mspres", "msdelay"};
+        dataorgs = {zmat (*Usual*), zlambdams (*Mean subtracted*), 
+          zlambdams (*Remove mean-Preserve \[Lambda]*), 
+          zlambdamsdel(*Remove mean,then delay*)};
+        {dataorgs, evalparts, flavours}
+    ] /; (! noisyQ)
 
-zlambdams = getmusubeddatamat[zmat,\[Lambda]];
-zlambdamsdel = Join@@Map[#[zlambdams\[Transpose]]\[Transpose]&,{Most,Rest}];
-
-evalparts = getevalpartitions[truevals,nfunda,n,\[Lambda],restols];
-flavours = {"vanilla","ms","mspres","msdelay"};
-dataorgs = {zmat (* Usual *),zlambdams (* Mean subtracted *),zlambdams (* Remove mean - Preserve \[Lambda]*),zlambdamsdel(* Remove mean, then delay *)};
-{dataorgs,evalparts,flavours}
-]/;(!noisyQ)
 
 
 
-
-prep4cases[{data_,noise_},\[Lambda]_,truevals_,sigtols_, restols_,nfunda_, ndelays_, deg_,meff_, noisyQ_]:=
-Module[{filterNgrub, zpure, znoisy, zTLS, zfilter, znoiseresist, n,evalparts,flavours,dataorgs, filter = <||>, grub = <||>},
-n = deg;
-(* Take the first meff and last n+1 columns please *)
-filterNgrub = {Take[#//Transpose, meff]//Transpose, Take[#//Transpose, -n-1]//Transpose}&;
-
-{filter["data"], grub["data"]} = filterNgrub[data];
-{filter["noise"], grub["noise"]} = filterNgrub[noise];
-(* Split data and noise into filter and actual grub *)
-
-(* Companion: Pure data *)
-zpure = grub["data"];
-
-(* Noisy companion: Data + Noise *)
-znoisy = zpure + grub["noise"];
-
-(* TLS Companion: Data + Noise --> SVD thresholding  *)
-zTLS = usvdot@(  (  pickysvd[znoisy, sigtols] )[[1]]);
-
-(* Noise resistant DMD: Noise added to both data matrices --> Inner \
-product with the average in front *)
-zfilter = filter["data"] + filter["noise"];
-znoiseresist = (1/(ndelays + 1))*ConjugateTranspose[zfilter].znoisy;
-
-evalparts = ConstantArray[{truevals, {}}, 4];
-flavours = ConstantArray["ms", 4];
-
-dataorgs = {zpure, znoisy, zTLS, znoiseresist};
-{dataorgs,evalparts,flavours}
-];
+prep4cases[{data_, noise_}, \[Lambda]_, truevals_, sigtols_, restols_,
+    nfunda_, ndelays_, deg_, meff_, noisyQ_] :=
+    Module[ {filterNgrub, zpure, znoisy, zTLS, zfilter, znoiseresist, n, evalparts, flavours, dataorgs, filter = <||>, grub = <||>, sigpicker},
+        n = deg;
+        (*Take the first meff and last n+1 columns please*)
+        filterNgrub = {Take[# // Transpose, meff] // Transpose, 
+           Take[# // Transpose, -n - 1] // Transpose} &;
+        {filter["data"], grub["data"]} = filterNgrub[data];
+        {filter["noise"], grub["noise"]} = filterNgrub[noise];
+        (*Split data and noise into filter and actual grub*)
+        (*Companion: Pure data*)
+        zpure = grub["data"];
+        (*Noisy companion:Data+Noise*)
+        znoisy = zpure + grub["noise"];
+        (*TLS Companion: Data+Noise-->SVD thresholding*)
+        sigpicker = 
+         If[ Length[truevals] > 0,
+         	(*Retain only as many singular values as there are Koopman EFs*)
+             Fold[sigthresholder, {#, UpTo[Length[truevals]], sigtols}] &,
+             sigthresholder[#, sigtols] &
+         ];
+        zTLS = lowrankapprox[znoisy, sigpicker];
+        (*Noise resistant DMD: Noise added to both data matrices --> Inner product with the average in front*)
+        zfilter = filter["data"] + filter["noise"];
+        znoiseresist = (1/(ndelays + 1))*
+          ConjugateTranspose[zfilter] . znoisy;
+        evalparts = ConstantArray[{truevals, {}}, 4];
+        flavours = ConstantArray["ms", 4];
+        dataorgs = {zpure, znoisy, zTLS, znoiseresist};
+        {dataorgs, evalparts, flavours}
+    ];
 
 
 updatednumchks[data_,\[Lambda]_,truevals_,restols_,sigtols_,nfunda_,ndelays_, deg_,meff_, noisyQ_,locquads_]:=
